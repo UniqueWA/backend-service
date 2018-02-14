6b3f397fba21aeaf14d95e6e1f31c5baa77bd53b
mistadrumma <breakroger104@gmail.com>
2018-02-14 20:19:58 +0300

    test

--- project/models.py
+++ project/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-
 from core.models import  AbstractModel
 
 # Create your models here.
@@ -15,13 +14,12 @@ LEVEL_CHOICES = (
 )
 
 class Project(AbstractModel):
-    # id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=250)
     date = models.DateTimeField(default=timezone.now)
     levelOfTraining = models.CharField(max_length=20, choices=LEVEL_CHOICES, default="medium")
     organizator = models.ForeignKey(Organizator, on_delete=models.CASCADE)
+    subscriber = models.ManyToManyField(User,through='Subscriber')
 
 
     def __str__(self):
         return self.name
-
